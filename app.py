98dff22f868628db597cd0146e6959d3ed0f7588
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2023-01-15 17:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system(".system("git clone https://darkphoenix2601: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@github.com/darkphoenix2601/demonking ok && cd ok && pip3 install -U -r requirements.txt && pip install --upgrade pip && nohup python3 -m YukkiMusic && pip install --upgrade pip")
+os.system("git clone https://darkphoenix2601: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@github.com/darkphoenix2601/demonking ok && cd ok && pip3 install -U -r requirements.txt && pip install --upgrade pip && nohup python3 -m YukkiMusic && pip install --upgrade pip")
